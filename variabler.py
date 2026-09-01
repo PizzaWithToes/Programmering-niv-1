5dee0d5d7f7e12edcfb2f7f75fddef5053bc3b98
Linus <322056181+PizzaWithToes@users.noreply.github.com>
2026-09-01 13:04:52 +0200

    Har gjort allting som stod i uppgiften!

---
+++ variabler.py
@@ -0,0 +1,24 @@
+ålder=(17)
+längd=(1.78)
+från=("Sverige")
+harGlasögon=(True)
+print(ålder)
+print(längd)
+print(från)
+print(harGlasögon)
+print(type(ålder))
+print(type(längd))
+print(type(från))
+print(type(harGlasögon))
+
+poäng = 0
+print(poäng)
+
+poäng += 10
+print(poäng)
+
+poäng += 10
+print(poäng)
+
+print(True + True)
+# Printar 2 eftersom True = 1 och False = 0!
