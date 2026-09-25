7289860d5f247bb1ae5b72f4697332779e3faf2c
Linus <322056181+PizzaWithToes@users.noreply.github.com>
2026-09-25 11:15:11 +0200

    Färger

---
+++ TextColor.py
@@ -0,0 +1,9 @@
+import colorama
+R = colorama.Fore.RED
+G = colorama.Fore.GREEN
+Y = colorama.Fore.YELLOW
+B = colorama.Fore.BLUE
+W = colorama.Fore.WHITE
+LB = colorama.Fore.LIGHTBLACK_EX
+BB = colorama.Back.BLACK
+RESET = colorama.Style.RESET_ALL
