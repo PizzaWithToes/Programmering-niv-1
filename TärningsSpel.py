410fd7f637ac5bd2449b40ba8116d1a0dc89415d
Linus <322056181+PizzaWithToes@users.noreply.github.com>
2026-09-25 11:14:13 +0200

    Fixade färger och ifall man skulle vilja spela igen

--- TärningsSpel.py
+++ TärningsSpel.py
@@ -1,21 +1,31 @@
-import random
+def TärningSpel():
+    import random
+    import TextColor
+    Total = int(0)
+    Försök = int(5)
+    Målet = int(50) 
+    while True:
+        input(f'{TextColor.BB}{TextColor.LB}Slå tärningen{TextColor.RESET}\n')
+        Försök -= 1
+        randomNumber = random.randint(1,20)
+        Total += randomNumber
+        print(f'Du har {TextColor.R}{Försök}{TextColor.RESET} försök kvar.\n')
+        print(f'Du rullade {TextColor.Y}{randomNumber}{TextColor.RESET} . Totalt ligger du på {TextColor.G}{Total}{TextColor.RESET}.\n')
+        Kvar = Målet - Total
+        if Kvar > 1:
+            print(f'Bara {TextColor.B}{Kvar}{TextColor.RESET} kvar!\n')
+        elif Kvar <= 0:
+            print(f'Bara 0 kvar!')
+        if Total >= Målet:
+            print(f'{TextColor.G}Du vann!{TextColor.RESET}')
+            break
+        elif Försök < 1:
+            print(f'{TextColor.R}Du förlorade{TextColor.RESET}')
+            break
+    Igen = input('Vill du köra igen?(Y/N)')
+    if Igen == 'Y':
+        return TärningSpel()
+    elif Igen == 'N':
+        print('ok!')
 
-Total = int(0)
-Försök = int(5)
-Målet = int(50)
-while True:
-    input('Slå tärningen')
-    Försök -= 1
-    randomNumber = random.randint(1,20)
-    Total += randomNumber
-    print(f'Du har {Försök} försök kvar.')
-    print(f'Du rullade {randomNumber}. Totalt ligger du på {Total}.')
-    Kvar = Målet - Total
-    print('Bara', Kvar,'kvar!')
-    if Total > Målet:
-        print('Du vann!')
-        break
-    elif Försök < 1:
-        print('Du förlorade')
-        break
-
+TärningSpel()
